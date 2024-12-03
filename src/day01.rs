f8c65fc2543031f3b951f34eb4fee16e9b2a0844
Brian Magnuson <bdmagnuson@gmail.com>
2024-12-02 21:01:26 -0500

    day1/day2

---
+++ src/day01.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "day01.pest"]
+struct Day01Parser;
+
+pub fn day01() {
+    let data = fs::read_to_string("input/day01.txt").expect("Unable to read file");
+    let file = Day01Parser::parse(Rule::file, &data)
+             .expect("parse failed")
+             .next().unwrap();
+
+    let pairs : Vec<(i32, i32)> =
+        file.into_inner().filter_map(|record| {
+            match record.as_rule() {
+                Rule::record => {
+                    let a : Vec<i32> = record.into_inner().map(|f| f.as_str().parse::<i32>().unwrap()).collect();
+                    Some((a[0], a[1]))
+                }
+                Rule::EOI => { None }
+                _ => unreachable!()
+            }
+        }).collect();
+    let (mut ls, mut  rs) : (Vec<i32>, Vec<i32>) = pairs.into_iter().unzip();
+    ls.sort();
+    rs.sort();
+    let sum : i32 = ls.clone().into_iter().zip(rs.clone()).map(|(l, r)| (l - r).abs()).sum();
+
+    let mut score = 0;
+    for l in ls {
+        score += (l as usize) * rs.clone().into_iter().filter(|r| *r == l).count()
+    }
+
+    println!("{:?}", sum);
+    println!("{:?}", score);
+}
---
+++ src/day02.rs
@@ -0,0 +1,46 @@
+use std::fs;
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "day02.pest"]
+struct Day02Parser;
+
+fn is_safe1 (v : &[i32]) -> bool {
+    let deltas : Vec<i32> = v.iter().zip(v[1..].iter()).map(|(l, r)| l - r).collect();
+    let alldecr = deltas.iter().all(|x| *x > 0);
+    let allincr = deltas.iter().all(|x| *x < 0);
+    let anydecr = deltas.iter().any(|x| *x > 0);
+    let anyincr = deltas.iter().any(|x| *x < 0);
+    let inrange = deltas.iter().all(|x| (x.abs() >= 1) && (x.abs() <= 3));
+    (alldecr || allincr) && !(anydecr && anyincr) && inrange
+}
+
+fn is_safe2 (v : &[i32]) -> bool {
+    let vs : Vec<Vec<i32>> = (0..v.len()).map(|x| { let mut _v = v.to_owned(); _v.remove(x); _v }).collect();
+    vs.iter().any(|v| is_safe1(v))
+}
+
+pub fn day02() {
+    let data = fs::read_to_string("input/day02.txt").expect("Unable to read file");
+    let file = Day02Parser::parse(Rule::file, &data)
+             .expect("parse failed")
+             .next().unwrap();
+
+    let reports : Vec<Vec<i32>> =
+        file.into_inner().filter_map(|record| {
+            match record.as_rule() {
+                Rule::record => {
+                    Some(record.into_inner().map(|f| f.as_str().parse::<i32>().unwrap()).collect())
+                }
+                Rule::EOI => { None }
+                _ => unreachable!()
+            }
+        }).collect();
+
+    let part1 : i32 = reports.iter().map(|v| if is_safe1(v) {1} else {0}).sum();
+    let part2 : i32 = reports.iter().map(|v| if is_safe2(v) {1} else {0}).sum();
+
+    println!("{:?}", part1);
+    println!("{:?}", part2);
+}
