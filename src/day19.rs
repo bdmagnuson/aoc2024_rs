bafd319b665051d49b35f3601c8dec8e1ceb1979
Brian Magnuson <bdmagnuson@gmail.com>
2024-12-30 12:02:31 -0500

    wrap up day 19

--- src/day19.rs
+++ src/day19.rs
@@ -2,6 +2,7 @@ use std::fs;
 use regex::Regex;
 use pest::Parser;
 use pest_derive::Parser;
+use memoize::memoize;
 
 #[derive(Parser)]
 #[grammar = "day19.pest"]
@@ -30,7 +31,26 @@ fn parse_input() -> (Vec::<String>, Vec<String>) {
     (towels, patterns)
 }
 
-fn part1(towels: &Vec::<String>, patterns: &Vec<String>) -> i32 {
+#[memoize]
+fn all_combs(pattern: String, towels: Vec<String>) -> Option<u64> {
+    let sum = towels.iter().filter_map(|t| {
+        match pattern.strip_prefix(t) {
+            Some("") => {
+                Some(1)
+            }
+            Some(s) => {
+                all_combs(s.to_string(), towels.clone())
+            }
+            None => {
+                None
+            }
+        }
+    }).collect::<Vec<_>>().iter().sum();
+    Some(sum)
+
+}
+
+fn part1(towels: &Vec::<String>, patterns: &Vec<String>) -> u64 {
     let re = Regex::new(&format!("^({})+$", towels.join("|"))).unwrap();
     let mut possible = 0;
     for p in patterns {
@@ -41,11 +61,13 @@ fn part1(towels: &Vec::<String>, patterns: &Vec<String>) -> i32 {
     possible
 }
 
-fn part2() -> i32 {
-    0
+fn part2(towels: &Vec::<String>, patterns: &Vec<String>) -> u64 {
+    patterns.iter().filter_map(|p| all_combs(p.clone(), towels.clone())).collect::<Vec<_>>().iter().sum()
 }
 
-pub fn day19() -> (i32, i32) {
+pub fn day19() -> (u64, u64) {
     let (towels, patterns) = parse_input();
-    (part1(&towels, &patterns), part2())
+    (part1(&towels, &patterns), part2(&towels, &patterns))
 }
+
+// 784859102 Low
