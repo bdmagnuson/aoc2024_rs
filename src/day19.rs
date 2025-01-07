1c8ca7424a07e5dd447ec3751f080701ae171e79
Brian Magnuson <bdmagnuson@gmail.com>
2025-01-07 08:48:10 -0500

    judicious use of unsage to go from 1s to 30ms

--- src/day19.rs
+++ src/day19.rs
@@ -8,6 +8,8 @@ use cached::proc_macro::cached;
 #[grammar = "day19.pest"]
 struct Day19Parser;
 
+static mut TOWELS_STATIC : Vec<String> = Vec::new();
+
 fn parse_input() -> (Vec::<String>, Vec<String>) {
     let data = fs::read_to_string("input/day19.txt").expect("Unable to read file");
     let file = Day19Parser::parse(Rule::file, &data)
@@ -28,24 +30,28 @@ fn parse_input() -> (Vec::<String>, Vec<String>) {
             _ => unreachable!()
         }
     }
+    unsafe { TOWELS_STATIC = towels.clone(); }
     (towels, patterns)
 }
 
+
 #[cached]
-fn all_combs<'a>(pattern: String, towels: Vec<String>) -> Option<u64> {
-    let sum = towels.iter().filter_map(|t| {
-        match pattern.strip_prefix(t) {
-            Some("") => {
-                Some(1)
+fn all_combs<'a>(pattern: String) -> Option<u64> {
+    let sum = unsafe {
+        TOWELS_STATIC.iter().filter_map(|t| {
+            match pattern.strip_prefix(t) {
+                Some("") => {
+                    Some(1)
+                }
+                Some(s) => {
+                    all_combs(s.to_string())
+                }
+                None => {
+                    None
+                }
             }
-            Some(s) => {
-                all_combs(s.to_string(), towels.clone())
-            }
-            None => {
-                None
-            }
-        }
-    }).collect::<Vec<_>>().iter().sum();
+        }).collect::<Vec<_>>().iter().sum()
+    };
     Some(sum)
 
 }
@@ -62,7 +68,9 @@ fn part1(towels: &[String], patterns: &Vec<String>) -> u64 {
 }
 
 fn part2(towels: &[String], patterns: &[String]) -> u64 {
-    patterns.iter().filter_map(|p| all_combs(p.clone(), towels.to_owned().clone())).collect::<Vec<_>>().iter().sum()
+    unsafe {
+        patterns.iter().filter_map(|p| all_combs(p.clone())).collect::<Vec<_>>().iter().sum()
+    }
 }
 
 pub fn day19() -> (u64, u64) {
@@ -70,4 +78,3 @@ pub fn day19() -> (u64, u64) {
     (part1(&towels, &patterns), part2(&towels, &patterns))
 }
 
-// 784859102 Low
